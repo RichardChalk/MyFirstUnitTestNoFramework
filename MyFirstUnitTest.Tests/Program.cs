015e249cdb418dfe2ad3d7a4258243b36de294ab
Richard Chalk <barmybrit@gmail.com>
2023-03-16 10:43:33 +0100

    Add project files.

---
+++ MyFirstUnitTest.Tests/Program.cs
@@ -0,0 +1,13 @@
+﻿using MyFirstUnitTest.Tests;
+
+Console.WriteLine("Hello, Here are my tests!!");
+
+// test 1
+CanIBuyBeerCalculatorTests.If_User_Is_Under18_And_Pub_Fail();
+
+// test 2
+CanIBuyBeerCalculatorTests.If_User_Is_Under20_And_Systemet_Fail();
+
+// test 3
+CanIBuyBeerCalculatorTests.If_User_Is_Too_Drunk_Fail();
+
